7dba1b87355bd127ffa7bb010e0d1cceff4f7436
Pdrj09 <pdrj09@users.noreply.github.com>
2026-02-09 21:40:09 +0100

    refactor: fixed some probles about linter

--- start/routes.ts
+++ start/routes.ts
@@ -15,31 +15,54 @@ router.on('/gracias').render('pages/gracias')
 router.get('/users', [() => import('#controllers/users_controller'), 'index'])
 
 // Authentication routes
-router.group(() => {
+router
+  .group(() => {
     router.get('/register', [() => import('#controllers/auth_controller'), 'showRegister'])
     router.post('/register', [() => import('#controllers/auth_controller'), 'register'])
     router.get('/login', [() => import('#controllers/auth_controller'), 'showLogin'])
     router.post('/login', [() => import('#controllers/auth_controller'), 'login'])
-    router.get('/auth/oidc/redirect', [() => import('#controllers/auth_controller'), 'oidcRedirect'])
-    router.get('/auth/oidc/callback', [() => import('#controllers/auth_controller'), 'oidcCallback'])
-}).use(middleware.guest())
+    router.get('/auth/oidc/redirect', [
+      () => import('#controllers/auth_controller'),
+      'oidcRedirect',
+    ])
+    router.get('/auth/oidc/callback', [
+      () => import('#controllers/auth_controller'),
+      'oidcCallback',
+    ])
+  })
+  .use(middleware.guest())
 
-router.post('/logout', [() => import('#controllers/auth_controller'), 'logout']).use(middleware.auth())
+router
+  .post('/logout', [() => import('#controllers/auth_controller'), 'logout'])
+  .use(middleware.auth())
 
 // Protected voting routes
-router.group(() => {
+router
+  .group(() => {
     router.get('/votacion', [() => import('#controllers/formulario_controller'), 'show'])
     router.post('/votacion', [() => import('#controllers/formulario_controller'), 'store'])
     router.get('/mi-voto', [() => import('#controllers/users_controller'), 'myVote'])
-}).use(middleware.auth())
+  })
+  .use(middleware.auth())
 
 // Admin protected routes
-router.group(() => {
+router
+  .group(() => {
     router.get('/consultar-voto', [() => import('#controllers/users_controller'), 'check'])
     router.post('/consultar-voto', [() => import('#controllers/users_controller'), 'find'])
     router.get('/consultar-voto/:id', [() => import('#controllers/users_controller'), 'show'])
 
-    router.get('/consultar-participante', [() => import('#controllers/users_controller'), 'checkParticipant'])
-    router.post('/consultar-participante', [() => import('#controllers/users_controller'), 'findParticipant'])
-    router.get('/consultar-participante/:id', [() => import('#controllers/users_controller'), 'showParticipant'])
-}).use(middleware.admin())
+    router.get('/consultar-participante', [
+      () => import('#controllers/users_controller'),
+      'checkParticipant',
+    ])
+    router.post('/consultar-participante', [
+      () => import('#controllers/users_controller'),
+      'findParticipant',
+    ])
+    router.get('/consultar-participante/:id', [
+      () => import('#controllers/users_controller'),
+      'showParticipant',
+    ])
+  })
+  .use(middleware.admin())
